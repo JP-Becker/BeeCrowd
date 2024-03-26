94afcf36ca085c3a71d4a1df82ca3a338ed8ee63
JP-Becker <jpbecker20@gmail.com>
2024-03-26 12:21:47 -0300

    Primeiros exercícios

---
+++ src/main/java/com/mycompany/beecrowd/BeeCrowd.java
@@ -0,0 +1,245 @@
+package com.mycompany.beecrowd;
+import java.util.Scanner;
+
+public class BeeCrowd {
+
+    public static void main(String[] args) {
+//        BEECROWD 1001 ->
+//        Scanner sc =  new Scanner(System.in);
+//        int a, b, x;
+//        
+//        System.out.println("Digite o primeiro número: ");
+//        a = Integer.parseInt(sc.nextLine());
+//        
+//        System.out.println("Digite o segundo número número: ");
+//        b = Integer.parseInt(sc.nextLine());
+//        
+//        x = a + b;
+//        
+//        System.out.println("X = " + x);
+//          
+//          BEECROWD 1002 ->
+//          Scanner sc =  new Scanner(System.in);
+//          double raio, area;
+//          ;;
+//          System.out.println("Digite o raio: ");
+//          raio = Double.parseDouble(sc.nextLine());
+//          
+//          area = 3.14159 *(raio*raio);
+//
+//          System.out.print("A=");
+//          System.out.printf("%.4f\n", area);
+
+//        BEECROWD 1003 ->
+//        Scanner sc =  new Scanner(System.in);
+//        int a, b, soma;
+//        
+//        System.out.println("Digite o primeiro número: ");
+//        a = Integer.parseInt(sc.nextLine());
+//        
+//        System.out.println("Digite o segundo número número: ");
+//        b = Integer.parseInt(sc.nextLine());
+//        
+//        soma = a + b;
+//        
+//        System.out.println("SOMA = " + soma);
+
+//        BEECROWD 1004 -> 
+//        Scanner sc =  new Scanner(System.in);;
+//        int a, b, prod;
+//        
+//        System.out.println("Digite o primeiro número: ");
+//        a = Integer.parseInt(sc.nextLine());
+//        
+//        System.out.println("Digite o segundo número número: ");
+//        b = Integer.parseInt(sc.nextLine());
+//        
+//        prod = a * b;
+//        
+//        System.out.println("PROD = " + prod);
+//
+//          BEECROWD 1005
+//          Scanner sc =  new Scanner(System.in);
+//          double a, b, media;
+//          
+//          System.out.println("Digite a primeira nota: ");
+//          a = Double.parseDouble(sc.nextLine());
+//          
+//          System.out.println("Digite a segunda nota: ");
+//          b = Double.parseDouble(sc.nextLine());
+//          
+//          media = ((a*3.5)+(b*7.5))/11;
+//          
+//          System.out.print("MEDIA = ");
+//          System.out.printf("%.5f\n", media);      
+//
+//          Scanner sc =  new Scanner(System.in);
+//          
+//          double a, b, c, media;
+//          
+
+//          BEECROWD 1006
+//          System.out.println("Digite a primeira nota: ");
+//          a = Double.parseDouble(sc.nextLine());             
+//          
+//          System.out.println("Digite a segunda nota: ");
+//          b = Double.parseDouble(sc.nextLine());
+//          
+//          System.out.println("Digite a terceira nota: ");
+//          c = Double.parseDouble(sc.nextLine());
+//          
+//          media = ((a*2)+(b*3) +(c*5))/10;
+//          
+//          System.out.print("MEDIA = ");
+//          System.out.printf("%.1f\n", media);   
+
+//
+//            BEECROWD 1007 -> 
+//            Scanner sc =  new Scanner(System.in);
+//            int a, b, c, d, formula;
+//            
+//            a = Integer.parseInt(sc.nextLine());
+//            b = Integer.parseInt(sc.nextLine());
+//            c = Integer.parseInt(sc.nextLine());
+//            d = Integer.parseInt(sc.nextLine());
+//            
+//            formula = (a*b - c*d);
+//            
+//            System.out.println("DIFERENCA = " + formula);
+
+
+
+// BEECROWD 1008
+//        Scanner sc =  new Scanner(System.in);
+//        int id, horasTrabalhadas;
+//        double valorHora, salario;
+//        
+//        id = Integer.parseInt(sc.nextLine());
+//        horasTrabalhadas = Integer.parseInt(sc.nextLine());
+//        valorHora = Double.parseDouble(sc.nextLine());
+//        
+//        salario = valorHora * horasTrabalhadas;
+//        
+//        System.out.println("NUMBER = " + id);
+//        System.out.print("SALARY = U$ ");
+//        System.out.printf("%.2f\n", salario);
+//
+
+
+
+//          BEECROWD 1009
+//        Scanner sc =  new Scanner(System.in);
+//        String nome;
+//        double salario, vendas, total;
+//        
+//        nome = sc.nextLine();
+//        
+//        salario = Double.parseDouble(sc.nextLine());
+//        vendas = Double.parseDouble(sc.nextLine());
+//        
+//        total = salario + (vendas * 0.15);
+//        
+//        System.out.print("TOTAL = R$ ");
+//        System.out.printf("%.2f\n", total);
+
+
+
+//          BEECROWD 1010
+//        Scanner sc =  new Scanner(System.in);
+//        
+//        int cod1 = sc.nextInt();
+//        int num1 = sc.nextInt();
+//        double valor1 = sc.nextDouble();
+//        
+//        int cod2 = sc.nextInt();
+//        int num2 = sc.nextInt();
+//        double valor2 = sc.nextDouble();
+//        
+//        double total = (num1 * valor1) + (num2 * valor2);
+//    
+//        System.out.print("VALOR A PAGAR: R$ ");
+//        System.out.printf("%.2f\n", total);
+//        
+
+//          BEECROWD 1011
+//        Scanner sc =  new Scanner(System.in);
+//        
+//        double raio = sc.nextDouble();
+//        
+//        double volume = (4.0/3) * 3.14159 * (Math.pow(raio, 3));
+//           
+//        System.out.printf("VOLUME = %.3f\n", volume);
+
+
+//          BEECROWD 1012
+//        Scanner sc =  new Scanner(System.in);
+//        double a, b, c;
+//        
+//        a = sc.nextDouble();
+//        
+//        b = sc.nextDouble();
+//        
+//        c = sc.nextDouble();
+//
+//        double areaTriangulo = a*c;
+//        double areaCirculo = c * 3.14159;
+//        double areaTrapezio = ((a + b)*c)/2;
+//        double areaQuadrado = b*b;
+//        double areaRetangulo = a*b;
+//        
+//        System.out.printf("TRIANGULO: %.3f\n", areaTriangulo);
+//        System.out.printf("CIRCULO: %.3f\n", areaCirculo);
+//        System.out.printf("TRAPEZIO: %.3f\n", areaTrapezio);
+//        System.out.printf("QUADRADO: %.3f\n", areaQuadrado);
+//        System.out.printf("RETANGULO: %.3f\n", areaRetangulo);
+
+//          BEECROWD 1013
+//           Scanner sc =  new Scanner(System.in);
+//           
+//           int a, b, c;
+//           
+//           a = sc.nextInt();
+//           b = sc.nextInt();
+//           c = sc.nextInt();
+//      
+//           int maiorAB = (a+b+Math.abs(a-b))/2;
+//           
+//           if (maiorAB > c) {
+//               System.out.println(maiorAB + " eh o maior");
+//           } else {
+//               System.out.println(c + " eh o maior");
+//           }
+
+
+
+//              BEECROWD 1014
+//            Scanner sc =  new Scanner(System.in);            
+//
+//            int x;
+//            double y;
+//            
+//            x = sc.nextInt();
+//            y = sc.nextDouble();
+//            
+//            double consumoMedio = x/y;
+//            
+//            System.out.printf("%.3f", consumoMedio);
+//            System.out.print(" km/l");
+
+
+            Scanner sc =  new Scanner(System.in); 
+            
+            int a, b, c, d;
+            
+             a = sc.nextInt();
+             b = sc.nextInt();
+             c = sc.nextInt();
+             d = sc.nextInt();
+             
+             if (b > c && d > a && c+d > a+b && c > 0 && d > 0 && a % 2 == 0) {
+                 System.out.println("Valores aceitos");
+             } else {
+                 System.out.println("Valores nao aceitos");
+             }
+    }
+}
