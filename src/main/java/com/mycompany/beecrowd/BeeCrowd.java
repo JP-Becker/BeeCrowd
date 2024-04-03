122f68a9d22033f714ea6b89d343fb85437dda6c
JP-Becker <jpbecker20@gmail.com>
2024-04-03 10:49:09 -0300

    Mais exercícios.

--- src/main/java/com/mycompany/beecrowd/BeeCrowd.java
+++ src/main/java/com/mycompany/beecrowd/BeeCrowd.java
@@ -227,19 +227,327 @@ public class BeeCrowd {
 //            System.out.print(" km/l");
 
 
-            Scanner sc =  new Scanner(System.in); 
-            
-            int a, b, c, d;
-            
-             a = sc.nextInt();
-             b = sc.nextInt();
-             c = sc.nextInt();
-             d = sc.nextInt();
+//            Scanner sc =  new Scanner(System.in); 
+//            
+//            int a, b, c, d;
+//            
+//             a = sc.nextInt();
+//             b = sc.nextInt();
+//             c = sc.nextInt();
+//             d = sc.nextInt();
+//             
+//             if (b > c && d > a && c+d > a+b && c > 0 && d > 0 && a % 2 == 0) {
+//                 System.out.println("Valores aceitos");
+//             } else {
+//                 System.out.println("Valores nao aceitos");
+//             }
              
-             if (b > c && d > a && c+d > a+b && c > 0 && d > 0 && a % 2 == 0) {
-                 System.out.println("Valores aceitos");
-             } else {
-                 System.out.println("Valores nao aceitos");
-             }
+                 public static void main(String[] args) {
+//        Scanner sc = new Scanner(System.in);
+//        
+//        System.out.println("digite o numero: ");
+//        int numero = sc.nextInt();
+//        
+//        switch (numero % 2) {
+//            case 0:
+//                System.out.println("é divisivel por 2");
+//                break;
+//            default:
+//                System.out.println("nao é divisivel");
+//                
+//        }
+//
+//
+//Scanner sc = new Scanner(System.in);
+//
+//        float valor;
+//        
+//        valor = sc.nextFloat();
+//        
+//        if (valor >= 0.0 && valor <= 25.0) {
+//            System.out.println("Intervalo [0,25]");
+//        } else if (valor >= 25.0 && valor <= 50.0) {
+//            System.out.println("Intervalo (25,50]");
+//        } else if (valor >= 50.0 && valor <= 75.0) {
+//            System.out.println("Intervalo (50,75]");
+//        } else if (valor >= 75.0 && valor <= 100.0) {
+//            System.out.println("Intervalo (75,100]");
+//        } else {
+//            System.out.println("Fora de intervalo");
+//        }
+//;
+//        Scanner sc = new Scanner(System.in);
+//        
+//        int n1, n2;
+//        
+//        System.out.println("primeiro numero: ");
+//        n1 = sc.nextInt();
+//        
+//        System.out.println("segundo numero: ");
+//        n2 = sc.nextInt();
+//        
+//        if (n1 > n2) {
+//            System.out.println("numero 1 é maior");
+//        } else if (n1 < n2) {
+//            System.out.println("numero 2 é maior");
+//        } else {
+//            System.out.println("Numeros iguais.");
+//        }
+           
+//        Scanner sc = new Scanner(System.in);
+//
+//        int DDD = sc.nextInt();
+//        
+//        switch (DDD) {
+//            case 61:
+//                System.out.println("Brasilia");
+//                break;
+//            case 71:
+//                System.out.println("Salvador");
+//                break;
+//            case 11:
+//                System.out.println("Sao Paulo");
+//                break;
+//            case 21:
+//                System.out.println("Rio de janeiro");
+//                break;
+//            case 32:
+//                System.out.println("Juiz de Fora");
+//                break;
+//            case 19:
+//                System.out.println("Campinas");
+//                break;
+//            case 27:
+//                System.out.println("Vitoria");
+//                break;
+//            case 31:
+//                System.out.println("Rio de Janeiro");
+//                break;
+//            default:
+//                System.out.println("DDD nao cadastrado");
+//        }
+//        
+//        Scanner sc = new Scanner(System.in);
+//        
+//        float x, y;
+//        
+//        x = sc.nextFloat();
+//        y = sc.nextFloat();
+//        
+//        if ( x > 0 && y > 0) {
+//            System.out.println("Q1");
+//        } else if (x< 0 && y > 0) {
+//            System.out.println("Q2");
+//        } else if (x< 0 && y < 0) {
+//            System.out.println("Q3");
+//        } else if (x > 0 && y < 0) {
+//            System.out.println("Q4");
+//        } else if (x == 0 && y == 0) {
+//            System.out.println("Origem");
+//        } else if (x == 0) {
+//            System.out.println("Eixo Y");
+//        } else if (y == 0) {
+//            System.out.println("Eixo X");
+//        }
+
+//        Scanner sc = new Scanner(System.in);
+//
+//        double prestacao, salario;
+//        
+//        System.out.println("salario: ");
+//        salario = sc.nextDouble();
+//        
+//        System.out.println("prestacao: ");
+//        prestacao = sc.nextDouble();
+//      
+//        
+//        if (prestacao > salario*0.3) {
+//            System.out.println("Valor maior que 30% do salario, emprestimo nao concedido.");
+//        } else {
+//            System.out.println("empréstimo concedido");
+//        }
+//        
+//        Scanner sc = new Scanner(System.in);
+//        
+//        int a, b;
+//        
+//        a = sc.nextInt();
+//        b = sc.nextInt();
+//
+//        if (a % b == 0) {
+//            System.out.println("Sao Multiplos");
+//        } else {
+//            System.out.println("Nao sao Multiplos");
+//        }
+
+//;
+//        Scanner sc = new Scanner(System.in);
+//        
+//        String nome, categoria;
+//        double preco;
+//        
+//        System.out.println("Digite seu nome");
+//        nome = sc.nextLine();
+//        
+//        System.out.println("Digite a categoria");
+//        categoria = sc.nextLine();
+//        
+//        System.out.println("Digite o preço do produto");
+//        preco = sc.nextDouble();
+//        
+//        switch (categoria) {
+//            case "a":
+//                preco = preco * 0.90;
+//                break;
+//            case "b":
+//                preco = preco * 0.85;
+//                break;
+//            case "c":
+//                preco = preco * 0.80;
+//                break;
+//            case "d":
+//                preco = preco * 0.75;
+//                break;
+//            case "e":
+//                preco = preco * 0.50;
+//                break;
+//            default:
+//                System.out.println("Não se encaixa em nenhuma categoria");
+//        }
+//        
+//        System.out.println(nome + "pagará um total de " + preco + " no produto");
+
+
+            /* CALCULADORA*/
+//        Scanner sc = new Scanner(System.in);
+//        
+//        double n1, n2, resultado = 0;
+//        int opcao;
+//        
+//        System.out.println("Digite o primeiro numero");
+//        n1 = sc.nextDouble();
+//        
+//        System.out.println("Digite o segundo numero");
+//        n2 = sc.nextDouble();
+//        
+//        System.out.println("Qual operação você quer realizar?");
+//        System.out.println("1 - Soma");
+//        System.out.println("2 - Subtração");
+//        System.out.println("3 - Multiplicação");
+//        System.out.println("4 - Divisão");
+//        
+//        opcao = sc.nextInt();
+//        
+//        switch (opcao) {
+//            case 1:
+//                resultado = n1+n2;
+//                break;
+//            case 2:
+//                resultado = n1-n2;
+//                break;
+//            case 3:
+//                resultado = n1*n2;
+//                break;
+//            case 4:
+//                resultado = n1/n2;
+//                break;
+//            default:
+//                System.out.println("Escolha uma opção válida!");
+//        }
+//        
+//        System.out.println("Resultado da operação: " + resultado);
+
+//          BEECROWD MÊS
+//        Scanner sc = new Scanner(System.in);
+//        
+//        int valor;
+//        
+//        valor = sc.nextInt();
+//        
+//        switch (valor) {
+//            case 1:
+//                System.out.println("January");
+//                break;
+//            case 2:
+//                System.out.println("February");
+//                break;
+//            case 3:
+//                System.out.println("March");
+//                break;
+//            case 4:
+//                System.out.println("April");
+//                break;
+//            case 5:
+//                System.out.println("May");
+//                break;
+//            case 6:
+//                System.out.println("June");
+//                break;
+//            case 7:
+//                System.out.println("July");
+//                break;
+//            case 8:
+//                System.out.println("August");
+//                break;
+//            case 9:
+//                System.out.println("September");
+//                break;
+//            case 10:
+//                System.out.println("October");
+//                break;
+//            case 11:
+//                System.out.println("November");
+//                break;
+//            case 12:
+//                System.out.println("December");
+//                break;
+//            default:
+//                System.out.println("Mês não existe!");
+//        }
+       
+//         Scanner sc = new Scanner(System.in);
+//        
+//         int x;
+//        
+//         x = sc.nextInt();
+         
+//         for (int i = 1; i <= 100; i++)  {
+//             if (i % 2 == 0) {
+//                 System.out.println(i);
+//             }
+//         }
+
+            Scanner sc = new Scanner(System.in);
+           
+            float a, b, c, d, e, f;
+            int qtd = 0;
+           
+            a = sc.nextFloat();
+            b = sc.nextFloat();
+            c = sc.nextFloat();
+            d = sc.nextFloat();
+            e = sc.nextFloat();
+            f = sc.nextFloat();
+           
+            if (a > 0) {
+                qtd += 1;
+            }
+            if (b > 0) {
+                qtd += 1;
+            }
+            if (c > 0) {
+                qtd += 1;
+            }
+            if (d > 0) {
+                qtd += 1;
+            }
+            if (e > 0) {
+                qtd += 1;
+            }
+            if (f > 0) {
+                qtd += 1;
+            }
+           
+            System.out.println(qtd + " valores positivos");
     }
 }
